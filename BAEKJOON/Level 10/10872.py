3f916e809c058d3f0754863c19435ace429c99b7
eungg <96607981+eungg@users.noreply.github.com>
2022-02-04 23:16:53 +0900

    Update 10872.py

--- BAEKJOON/Level 10/10872.py
+++ BAEKJOON/Level 10/10872.py
@@ -9,3 +9,14 @@ for i in range(1,n+1):
     result*=i
 
 print(result)
+
+
+# 이게 재귀함수래
+def factorial(n):
+    result = 1
+    if n > 0 :
+        result = n * factorial(n-1)
+    return result
+
+n = int(input())
+print(factorial(n))
